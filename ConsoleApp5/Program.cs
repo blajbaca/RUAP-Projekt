7bb7f814a70b2c415e5621b98f0e70c03279ff41
Nikola <mdamjanovic2010@gmail.com>
2022-10-25 17:34:18 +0200

    Treci

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -8,7 +8,10 @@ namespace ConsoleApp5
         {
             Console.WriteLine("Pozdrav iz aplikacije od Brune!");
 <<<<<<< HEAD
+<<<<<<< HEAD
             Console.WriteLine("12837192");
+=======
+>>>>>>> c307fea (Prvi commit zad 1.7)
 =======
 >>>>>>> c307fea (Prvi commit zad 1.7)
         }
