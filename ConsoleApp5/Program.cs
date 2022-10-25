15b9311d30a2fd366996a807d9b261189b7931a3
Nikola <mdamjanovic2010@gmail.com>
2022-10-25 18:13:02 +0200

    Treci zad

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -7,13 +7,7 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             Console.WriteLine("Pozdrav iz aplikacije od Brune!");
-<<<<<<< HEAD
-<<<<<<< HEAD
-            Console.WriteLine("12837192");
-=======
->>>>>>> c307fea (Prvi commit zad 1.7)
-=======
->>>>>>> c307fea (Prvi commit zad 1.7)
+
         }
     }
 }
